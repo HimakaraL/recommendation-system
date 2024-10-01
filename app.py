a6c42b8660503a659d46088b3bcf40a52f2fa792
HimakaraL <himakaral@outlook.com>
2024-10-01 18:06:37 +0530

    Collaborative filtering added
    10.01 Evening

--- app.py
+++ app.py
@@ -29,6 +29,83 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+# Content-based recommendation system
+def content_based_recommendations(train_data, item_name, top_n):
+    # Checking if the item exists in the dataset
+    if item_name not in train_data['Name'].values:
+        return pd.DataFrame()
+
+    # Create a TF-IDF vectorizer for the 'Tags' column
+    tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+    tfidf_matrix_content = tfidf_vectorizer.fit_transform(train_data['Tags'])
+
+    # Calculate cosine similarity
+    cosine_similarities_content = cosine_similarity(tfidf_matrix_content, tfidf_matrix_content)
+
+    # Find item index
+    item_index = train_data[train_data['Name'] == item_name].index[0]
+
+    # Get cosine similarity scores
+    similar_items = list(enumerate(cosine_similarities_content[item_index]))
+
+    # Sort items by similarity score
+    similar_items = sorted(similar_items, key=lambda x: x[1], reverse=True)
+
+    # Get top N most similar items (excluding the item itself)
+    top_similar_items = similar_items[1:top_n+1]
+    recommended_item_indices = [x[0] for x in top_similar_items]
+
+    # Fetch details of recommended items
+    recommended_item_details = train_data.iloc[recommended_item_indices][['Name', 'EpisodeCount', 'Genre', 'ImageURL']]
+
+    return recommended_item_details
+
+# Bought items
+def bought_items_details(train_data, bought_items):
+    if bought_items:
+        bought_items = bought_items.split(',')
+        bought_items_indices = train_data[train_data['Name'].isin(bought_items)].index
+        bought_items_details = train_data.iloc[bought_items_indices][['Name', 'EpisodeCount', 'Genre', 'ImageURL', 'Rating']]
+        # Convert the DataFrame to a list of dictionaries
+        bought_items_details = bought_items_details.to_dict('records')
+    return bought_items_details
+
+# Collaborative filtering recommendation system
+def collborative_filtering_recommendations(train_data, target_user_id, top_n=10):
+    #Create the user_item_matrix 
+    user_item_matrix=train_data.pivot_table(index='UserID', columns='AnimeID', values='Rating', aggfunc='mean').fillna(0).astype(int)
+
+    #Calculate the user similarity matrix using cosine similarity
+    user_similarity = cosine_similarity(user_item_matrix)
+
+    #Find the index of the target user in the matrix
+    target_user_index = user_item_matrix.index.get_loc(target_user_id)
+
+    #Get the similarity scores for the target user
+    user_similarities = user_similarity[target_user_index]
+
+    #Sort the users by similarity in descending order
+    similar_users_indices = user_similarities.argsort()[::-1][1:]
+
+    #Generate recommendation based on similarities
+    recommended_items = []
+
+    for user_index in similar_users_indices:
+        #Get items rated by the similar users
+        rated_by_similar_user = user_item_matrix.iloc[user_index]
+        not_rated_by_target_user = (rated_by_similar_user == 0) & (user_item_matrix.iloc[target_user_index])
+
+        #Extract the item IDs
+        recommended_items.extend(user_item_matrix.columns[not_rated_by_target_user][:top_n])
+
+    #Get the details of recommended items
+    recommended_items_details = train_data[train_data['AnimeID'].isin(recommended_items)][['Name', 'EpisodeCount', 'Genre', 'ImageURL', 'Rating']]
+
+    recommended_items_details = recommended_items_details.to_dict('records')
+
+    return recommended_items_details
+    
+
 @app.context_processor
 def inject_user():
     return dict(current_user=current_user)
@@ -108,7 +185,18 @@ def logout():
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('dashboard.html', user=current_user)
+    bought_items = bought_items_details(train_data, current_user.bought_items)
+
+    user_bought_items = current_user.bought_items
+    bought_items_for_collaborative = user_bought_items.split(',')
+    bought_items_indices = train_data[train_data['Name'].isin(bought_items_for_collaborative)].index
+
+    relavant_anime = train_data.loc[bought_items_indices].sample(1)  # Now sampling from DataFrame, not Index
+    user_id_for_collaborative = relavant_anime['UserID'].values[0] 
+
+    collaborative_recommendations = collborative_filtering_recommendations(train_data, user_id_for_collaborative)
+
+    return render_template('dashboard.html', user=current_user, bought_items_details=bought_items, collaborative_animes=collaborative_recommendations)
 
 @app.route('/buy_item', methods=['POST'])
 @login_required
@@ -123,37 +211,6 @@ def buy_item():
     flash(f'Bought item: {item}', 'success')
     return redirect(url_for('dashboard'))
 
-# Content-based recommendation system
-def content_based_recommendations(train_data, item_name, top_n):
-    # Checking if the item exists in the dataset
-    if item_name not in train_data['Name'].values:
-        return pd.DataFrame()
-
-    # Create a TF-IDF vectorizer for the 'Tags' column
-    tfidf_vectorizer = TfidfVectorizer(stop_words='english')
-    tfidf_matrix_content = tfidf_vectorizer.fit_transform(train_data['Tags'])
-
-    # Calculate cosine similarity
-    cosine_similarities_content = cosine_similarity(tfidf_matrix_content, tfidf_matrix_content)
-
-    # Find item index
-    item_index = train_data[train_data['Name'] == item_name].index[0]
-
-    # Get cosine similarity scores
-    similar_items = list(enumerate(cosine_similarities_content[item_index]))
-
-    # Sort items by similarity score
-    similar_items = sorted(similar_items, key=lambda x: x[1], reverse=True)
-
-    # Get top N most similar items (excluding the item itself)
-    top_similar_items = similar_items[1:top_n+1]
-    recommended_item_indices = [x[0] for x in top_similar_items]
-
-    # Fetch details of recommended items
-    recommended_item_details = train_data.iloc[recommended_item_indices][['Name', 'EpisodeCount', 'Genre', 'ImageURL']]
-
-    return recommended_item_details
-
 # Routes
 @app.route('/main')
 def main():
@@ -167,7 +224,7 @@ def recommend():
         content_based_rec = content_based_recommendations(train_data, anime_name, anime_count)
 
         if content_based_rec.empty:
-            return render_template('main.html', message='Anime not found in the dataset')
+            return render_template('main.html', message='Anime not found!')
         else:
             recommend_animes = content_based_rec.to_dict(orient='records')
             return render_template('main.html', recommend_animes=recommend_animes)
